146e6920ab717179598d356758bcfd54ccbfe1e1
Rosalio404 <Rosalio404@protonmail.com>
2020-09-12 23:49:50 -0500

    replace switch, add list adding functionality

--- app.js
+++ app.js
@@ -5,50 +5,32 @@ const bodyParser = require("body-parser");
 //Variables
 const app = express();
 const port = 3000;
+var items = [];
+		  
+//Body-Parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Express
 app.set("view engine", "ejs");
 
 //GET
 app.get("/", function(req, res){
-
-		var today = new Date();
-		var currentDay = today.getDay();
-		var day = "";
-
-		switch (currentDay) {
-				case 0:
-						day = "Sunday";
-						break;
-				case 1:
-						day = "Monday";
-						break;
-				case 2:
-						day = "Tuesday";
-						break;
-				case 3:
-						day = "Wednesday";
-						break;
-				case 4:
-						day = "Thursday";
-						break;
-				case 5:
-						day = "Friday";
-						break;
-				case 6:
-						day = "Saturday";
-						break;
-				default:
-						console.log("Error: currentDay is equal to " + currentDay)
-		}
-		// if (currentDay === 6 || currentDay === 0) {
-		// 		var day = "Weekend";
-		// } else {
-		// 		var day = "Weekday";
-		// }
-		res.render("list", {kindOfDay: day});
+	var today = new Date();
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
+	var day = today.toLocaleDateString("en-US", options);
+	res.render("list", {kindOfDay: day, newListItems: items});
 });
 
+//POST
+app.post("/", function(req, res){
+	var newItem = req.body.newitem;
+	items.push(newItem)
+	res.redirect("/");
+})
 //Listen
 app.listen(port, function(){
 		console.log("Server started on port " + port);
