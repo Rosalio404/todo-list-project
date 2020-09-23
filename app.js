4e39b4cca8b85955d8f86e1de2bc9f4a65a05e65
Rosalio404 <Rosalio404@protonmail.com>
2020-09-22 21:11:50 -0500

    Change for V2 starter files

--- app.js
+++ app.js
@@ -1,51 +1,48 @@
-//Modules
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
-//Variables
 const app = express();
-const port = 3000;
-const items = ["Buy food", "Cook food", "Eat food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-//Body-Parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res) {
 
-//Express
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+const day = date.getDate();
 
-//GET
-app.get("/", function(req, res){
-	let route = "/";
-	const day = date.getDate();
-	res.render("list", {listTitle: day, newListItems: items, route: route});
+	res.render("list", {listTitle: day, newListItems: items});
+
 });
 
-app.get("/work", function(req, res){
-	let route = "/work";
-	res.render("list", {listTitle: "Work", newListItems: workItems, route: route});
-});
-
-app.get("/about", function(req, res){
-	res.render("about")
-});
-
-//POST
 app.post("/", function(req, res){
-	const item = req.body.newitem;
+
+	const item = req.body.newItem;
 
 	if (req.body.list === "Work") {
-	workItems.push(item);
-	res.redirect("/work");
+		workItems.push(item);
+		res.redirect("/work");
 	} else {
-	items.push(item);
-	res.redirect("/");
+		items.push(item);
+		res.redirect("/");
 	}
 });
 
-//Listen
-app.listen(port, function(){
-		console.log("Server started on port " + port);
+app.get("/work", function(req,res){
+	res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+
+app.listen(3000, function() {
+	console.log("Server started on port 3000");
 });
--- date.js
+++ date.js
@@ -1,11 +1,27 @@
+//jshint esversion:6
 
-// Function to retrieve the current date
-exports.getDate = function () {
-		const today = new Date();
-		const options = {
-				weekday: "long",
-				day: "numeric",
-				month: "long",
-		};
-		return today.toLocaleDateString("en-US", options);
-}
+exports.getDate = function() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
+
+exports.getDay = function () {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
